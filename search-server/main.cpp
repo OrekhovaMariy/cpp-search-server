cf4ef2a05ff15d8c4856ea91ee3e116f35bf841f
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2021-12-09 12:43:00 +0300

    answer
    answer

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,27 +1,4 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-int main() {
-	vector<string> a;
-	for (int x = 1; x < 1000; ++x) {
-		string b = to_string(x);
-		a.push_back(b);
-	}
-	int count = 0;
-	for (int i = 0; i < a.size(); ++i) {
-		for (char z : a[i]) {
-				if (z == '3') {
-					count += 1;
-				}
-			}
-		}
-	cout << count << endl;
-	return 0;
-	}
+271
 // Закомитьте изменения и отправьте их в свой репозиторий.
