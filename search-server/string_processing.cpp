40c4335973b42064d6243c1673eec0f1f43f8f59
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-04-19 09:10:41 +0300

    8 спринт итоговая работа
    8 спринт итоговая работа

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <string_view>
+#include <execution>
 
 #include "request_queue.h"
 #include "read_input_functions.h"
+#include "string_processing.h"
 
 
 int ReadLineWithNumber() {
     int result;
     std::cin >> result;
     ReadLine();
+    return result;
+}
+
+std::vector<std::string_view> SplitIntoWords(std::string_view str) {
+    std::vector<std::string_view> result;
+    while (true) {
+        const auto space = str.find(' ');
+        result.push_back(str.substr(0, space));
+        if (space == str.npos) {
+            break;
+        }
+        else {
+            str.remove_prefix(space + 1);
+        }
+    }
     return result;
 }
 
