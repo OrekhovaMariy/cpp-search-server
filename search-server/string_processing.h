fcf4a6c16c2a78dfc88955657a85ade552d0e73e
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-04-19 09:10:11 +0300

    8 спринт итоговая работа
    8 спринт итоговая работа

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -5,16 +5,17 @@
 
 
 int ReadLineWithNumber();
-std::vector<std::string> SplitIntoWords(const std::string& text);
+std::vector<std::string_view> SplitIntoWords(std::string_view text);
+
+using TransparentStringSet = std::set<std::string, std::less<>>;
 
 template <typename StringContainer>
-std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<std::string> non_empty_strings;
-    for (const std::string& str : strings) {
+TransparentStringSet MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    TransparentStringSet non_empty_strings;
+    for (std::string_view str : strings) {
         if (!str.empty()) {
-            non_empty_strings.insert(str);
+            non_empty_strings.emplace(str);
         }
     }
     return non_empty_strings;
 }
-
