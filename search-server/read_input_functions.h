01134556b095780dcb2d65869a6d3d13228310cb
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-04-19 09:12:53 +0300

    8 спринт итоговая работа
    8 спринт итоговая работа

--- search-server/read_input_functions.h
+++ search-server/read_input_functions.h
@@ -5,11 +5,11 @@
 #include "search_server.h"
 
 std::string ReadLine();
-void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status);
-void AddDocument(SearchServer& search_server, int document_id, const std::string& document,
+void PrintMatchDocumentResult(int document_id, const std::vector<std::string_view> words, DocumentStatus status);
+void AddDocument(SearchServer& search_server, int document_id, std::string_view document,
     DocumentStatus status, const std::vector<int>& ratings);
 
-void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
+void FindTopDocuments(const SearchServer& search_server, std::string_view raw_query);
 
-void MatchDocuments(const SearchServer& search_server, const std::string& query);
+void MatchDocuments(const SearchServer& search_server, std::string_view query);
 
