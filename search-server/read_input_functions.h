8bf6b7116111b62b4d7e568938972425d7cd4cf7
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-04-20 06:36:39 +0300

    8 спринт итоговая работа - внесены правки
    8 спринт итоговая работа - внесены правки

--- search-server/read_input_functions.h
+++ search-server/read_input_functions.h
@@ -1,15 +1,5 @@
 #pragma once
 #include <string>
-#include <vector>
-
-#include "search_server.h"
 
 std::string ReadLine();
-void PrintMatchDocumentResult(int document_id, const std::vector<std::string_view> words, DocumentStatus status);
-void AddDocument(SearchServer& search_server, int document_id, std::string_view document,
-    DocumentStatus status, const std::vector<int>& ratings);
-
-void FindTopDocuments(const SearchServer& search_server, std::string_view raw_query);
-
-void MatchDocuments(const SearchServer& search_server, std::string_view query);
-
+int ReadLineWithNumber();
